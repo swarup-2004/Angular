c5285b018736a2985a6a4412d7ed710eff148cf2
Swarup Pokharkar <swaruppokharkar8@gmail.com>
2025-01-20 20:52:58 +0530

    Complete RxJS

--- 10.RxJS/01-starting-project/src/app/app.component.ts
+++ 10.RxJS/01-starting-project/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { interval, map, Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  private destroyRef = inject(DestroyRef);
+  ngOnInit(): void {
+
+    interval(1000).pipe(
+      map((val) => val * 2)
+    ).subscribe({
+      next: (val) => console.log(val)
+    });
+
+      
+  }
 
 }
