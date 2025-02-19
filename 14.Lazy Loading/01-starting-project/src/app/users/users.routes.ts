68f55a499bb0d22ed2496008e5ea151d1f01d742
Swarup Vijay Pokharkar <swaruppokharkar8@gmail.com>
2025-02-19 14:23:41 +0530

    Add and  complete lazy loading

---
+++ 14.Lazy Loading/01-starting-project/src/app/users/users.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+
+import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
+import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full',
+  },
+  {
+    path: 'tasks', // <your-domain>/users/<uid>/tasks
+    loadComponent: () => import('../tasks/tasks.component').then(mod => mod.TasksComponent),
+    runGuardsAndResolvers: 'always',
+    resolve: {
+      userTasks: resolveUserTasks,
+    },
+  },
+  {
+    path: 'tasks/new',
+    component: NewTaskComponent,
+    canDeactivate: [canLeaveEditPage]
+  },
+];
